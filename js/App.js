d9bb743ed1a3a9e229815760cd41560512c02fbf
Dorota Adamska <dorota.sobel@gmail.com>
2019-05-23 09:41:37 +0200

    Modified App.js

--- js/App.js
+++ js/App.js
@@ -1,11 +1,33 @@
-    function randomString() {
-        var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
-        var str = '';
-        for (var i = 0; i < 10; i++) {
-            str += chars[Math.floor(Math.random() * chars.length)];
-        }
-        return str;
-    }
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': 'X-Client-Id',
+  'X-Auth-Token': 'X-Auth-Token'
+}; 
+
+fetch(prefix + baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+  });
+    
+  function setupColumns(columns) {
+    columns.forEach(function (column) {
+          var col = new Column(column.id, column.name);
+      board.addColumn(col);
+      setupCards(col, column.cards);
+    });
+  }
+
+  function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
+
     
     function generateTemplate(name, data, basicElement) {
         var template = document.getElementById(name).innerHTML;
